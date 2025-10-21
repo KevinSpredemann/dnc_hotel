7fe869193d1e9f7c9f0ac295443a9990d3a9c375
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2025-10-20 21:39:59 -0300

    feat: prisma config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,20 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { fileURLToPath } = require('url');
+const path = require('path');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default {
+module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -21,8 +23,18 @@ export default {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: [
+    '.eslintrc.cjs',
+    'dist',
+    'node_modules',
+  ],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
