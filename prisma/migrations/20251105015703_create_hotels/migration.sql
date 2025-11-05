f078f665caa2d852daea142e83da79e2f6f49b3f
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2025-11-04 22:57:37 -0300

    feat: create model Hotel

---
+++ prisma/migrations/20251105015703_create_hotels/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "hotels" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "image" TEXT,
+    "price" REAL NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
